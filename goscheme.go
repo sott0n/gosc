57922fcd8b24c056e067eb0e15c7b52a5e07a602
sotoshigoto <fix7211@gmail.com>
2018-03-03 13:43:34 +0900

    Fix bugs.

--- goscheme.go
+++ goscheme.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"github.com/sott0n/go-scheme/scheme"
-)
-
 func main() {
 	// TODO: parse args
 }
--- scheme/const.go
+++ scheme/const.go
@@ -3,9 +3,9 @@ package scheme
 type Const SchemeType
 
 func NewConst(expression string) *Const {
-	return &Const(
+	return &Const{
 		expression: expression,
-	)
+	}
 }
 
 func (c *Const) String() string {
