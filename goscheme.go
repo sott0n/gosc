f130d467d1af74f2906d2bc1a1611c1f2444dfce
sotoshigoto <fix7211@gmail.com>
2018-03-08 02:43:39 +0900

    Create a REPL that is interactive shell prototype.

--- goscheme.go
+++ goscheme.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func repl(indentLevel int) {
+	if indentLevel == 0 {
+		fmt.Print("gosc>> ")
+	} else if indentLevel > 0 {
+		fmt.Print("... ")
+		for length := indentLevel; length > 0; length-- {
+			fmt.Print("  ")
+		}
+	} else {
+		panic("Negative indent level.")
+	}
+}
+
+func invokeInteractiveShell() {
+	commandLine := bufio.NewReader(os.Stdin)
+	for {
+		indentLevel := 0
+		expression := ""
+		for {
+			repl(indentLevel)
+			currentLine, err := commandLine.ReadString("\n")
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			expression += currentLine
+
+			interpreter := scheme.NewInterpreter(expression)
+			if indentLevel = interpreter.IndentLevel(); indentLevel == 0 {
+				interpreter.Eval()
+				break
+			}
+		}
+	}
+}
+
 func main() {
-	// TODO: parse args
+	invokeInteractiveShell()
 }
---
+++ scheme/lexer.go
@@ -0,0 +1,16 @@
+package scheme
+
+import (
+	"io"
+	"text/scanner"
+)
+
+type Lexer struct {
+	scanner.Scanner
+}
+
+func NewLexer(source io.Reader) *Lexer {
+	lexer := Lexer{}
+	lexer.Init(source)
+	return &lexer
+}
