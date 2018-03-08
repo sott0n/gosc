bb2f960cc868efc13b93d7b61192e4d49da8a851
sotoshigoto <fix7211@gmail.com>
2018-03-09 05:57:18 +0900

    Support number parsing.

--- scheme/parser_test.go
+++ scheme/parser_test.go
@@ -9,11 +9,18 @@ type parserTest struct {
 
 var parserTests = []parserTest{
 	{"()", "()"},
+	{"10", "10"},
+	{"123456789", "123456789"},
 }
 
 func TestParser(t *testing.T) {
 	for _, test := range parserTests {
-		actual := NewParser(test.source).Parse().String()
+		parseObject := NewParser(test.source).Parse()
+		if parseObject == nil {
+			t.Errorf("%s => <nil>; want %s", test.source, test.result)
+			return
+		}
+		actual := parseObject.String()
 		if actual != test.result {
 			t.Errorf("%s => %s, want %s", test.source, actual, test.result)
 		}
