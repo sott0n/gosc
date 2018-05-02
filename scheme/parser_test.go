519ee8ba3888f24c13b600f2239aae08bb9c3d87
Kohei Yamaguchi <fix7211@gmail.com>
2018-05-03 05:32:26 +0900

    Accept multiple parser test results.

--- scheme/parser_test.go
+++ scheme/parser_test.go
@@ -5,45 +5,52 @@ import (
 )
 
 type parserTest struct {
-	source string
-	result string
+	source  string
+	results []string
 }
 
 var parserTests = []parserTest{
-	{"()", "()"},
-	{"#f", "#f"},
-	{"#t", "#t"},
-	{"1234567890", "1234567890"},
-	{"(+)", "0"},
-	{"(- 1)", "1"},
-	{"(*)", "1"},
-	{"(/ 1)", "1"},
-	{"(+ 1 2)", "3"},
-	{"(+ 1 20 300 4000)", "4321"},
-	{"( + 1 2 3 )", "6"},
-	{"(+ 1 (+ 2 3) (+ 3 4))", "13"},
-	{"(- 3 (- 2 3) (+ 3 0))", "1"},
-	{"(* (* 3 3) 3)", "27"},
-	{"(/ 100 (/ 4 2))", "50"},
-	{"(+ (* 100 3) (/ (- 4 2) 2))", "301"},
-	{"(number? 100)", "#t"},
-	{"(number? (+ 3 (* 2 8)))", "#t"},
-	{"(number? #t)", "#f"},
-	{"(number? ()", "#f"},
+	makePT("()", "()"),
+	makePT("#f", "#f"),
+	makePT("#t", "#t"),
+	makePT("1234567890", "1234567890"),
+	makePT("(+)", "0"),
+	makePT("(- 1)", "1"),
+	makePT("(*)", "1"),
+	makePT("(/ 1)", "1"),
+	makePT("(+ 1 2)", "3"),
+	makePT("(+ 1 20 300 4000)", "4321"),
+	makePT("( + 1 2 3 )", "6"),
+	makePT("(+ 1 (+ 2 3) (+ 3 4))", "13"),
+	makePT("(- 3 (- 2 3) (+ 3 0))", "1"),
+	makePT("(* (* 3 3) 3)", "27"),
+	makePT("(/ 100 (/ 4 2))", "50"),
+	makePT("(+ (* 100 3) (/ (- 4 2) 2))", "301"),
+	makePT("(number? 100)", "#t"),
+	makePT("(number? (+ 3 (* 2 8)))", "#t"),
+	makePT("(number? #t)", "#f"),
+	makePT("(number? ()", "#f"),
+}
+
+func makePT(source string, results ...string) parserTest {
+	return parserTest{source: source, results: results}
 }
 
 func TestParser(t *testing.T) {
 	for _, test := range parserTests {
 		p := NewParser(test.source)
 		p.Peek()
-		parseObject := p.Parse()
-		if parseObject == nil {
-			t.Errorf("%s => <nil>; want %s", test.source, test.result)
-			return
-		}
-		actual := parseObject.String()
-		if actual != test.result {
-			t.Errorf("%s => %s; want %s", test.source, actual, test.result)
+		for i := 0; i < len(test.results); i++ {
+			result := test.results[i]
+			parseObject := p.Parse()
+			if parseObject == nil {
+				t.Errorf("%s => <nil>; want %s", test.source, result)
+				return
+			}
+			actual := parseObject.String()
+			if actual != result {
+				t.Errorf("%s => %s; want %s", test.source, actual, result)
+			}
 		}
 	}
 }
