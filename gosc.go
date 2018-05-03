1b278255c0117b64ca2f1344dc0f6cd3f08448c0
Kohei Yamaguchi <fix7211@gmail.com>
2018-05-03 19:53:20 +0900

    Implemented options parser into REPL.

--- gosc.go
+++ gosc.go
@@ -2,24 +2,43 @@ package main
 
 import (
 	"fmt"
-	"go-scheme/scheme"
+	"gosc/scheme"
+	"io/ioutil"
 	"log"
 	"strings"
 
 	"github.com/GeertJohan/go.linenoise"
+	"github.com/jessevdk/go-flags"
 )
 
-func callRepl(indentLevel int) string {
-	if indentLevel == 0 {
-		return "gosc>> "
-	} else if indentLevel > 0 {
-		return fmt.Sprintf("... %s", strings.Repeat("  ", indentLevel))
+// Options is definition gosc's selection option.
+type Options struct {
+	FileName string `short:"f" long:"file" description:"interpret selected scheme source file"`
+	DumpAST  bool   `short:"a" long:"ast" default:"false" description:"whether leafnodes are plotte"`
+}
+
+func main() {
+	options := new(Options)
+	if _, err := flags.Parse(options); err != nil {
+		return
+	}
+	if len(options.FileName) > 0 {
+		executeSourceCode(options)
 	} else {
-		panic("Negative indent level.")
+		repl(options)
 	}
 }
 
-func repl() {
+func executeSourceCode(options *Options) {
+	buffer, err := ioutil.ReadFile(options.FileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	interpreter := scheme.NewInterpreter(string(buffer))
+	interpreter.Eval(options.DumpAST)
+}
+
+func repl(options *Options) {
 	for {
 		indentLevel := 0
 		expression := ""
@@ -40,7 +59,8 @@ func repl() {
 			if indentLevel == 0 {
 				// Because the IndentLevel() method changes its reading position,
 				// recreate intepreter to initialize the position.
-				interpreter.Eval()
+				interpreter := scheme.NewInterpreter(expression)
+				interpreter.Eval(options.DumpAST)
 				break
 			} else if indentLevel < 0 {
 				log.Println("Error: extra close parentheses.")
@@ -51,6 +71,12 @@ func repl() {
 	}
 }
 
-func main() {
-	repl()
+func callRepl(indentLevel int) string {
+	if indentLevel == 0 {
+		return "gosc>> "
+	} else if indentLevel > 0 {
+		return fmt.Sprintf("... %s", strings.Repeat("  ", indentLevel))
+	} else {
+		panic("Negative indent level.")
+	}
 }
